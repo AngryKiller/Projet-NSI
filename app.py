bca1b7e9e1dc8271123614caaf3303ad1c64cc4e
AngryKiller <angrykiller@icloud.com>
2021-02-10 21:29:58 +0100

    Added news deletion in the admin panel

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash, session
-from util.news import getNews
+from util.news import getNews, deleteNews
 from util.users import register, login, getUser, isAdmin
 
 app = Flask(__name__, static_url_path='/static')
@@ -60,6 +60,18 @@ def adminNewsRoute():
         return render_template('admin/news.html', user=getUser(session['user']), news=getNews())
     else:
         return redirect('/')
+
+@app.route('/admin/news/delete')
+def deletenewsroute():
+    if 'user' in session and isAdmin(session['user']):
+        id = request.args.get('id', '')
+        deleteNews(id)
+        flash("L'actualité a été supprimée", 'success')
+        return redirect('/admin/news')
+    else:
+        return redirect('/')
+
+
 
 @app.route('/digou')
 def test():
--- util/news.py
+++ util/news.py
@@ -10,3 +10,10 @@ def getNews():
     cursor = cnx.cursor()
     cursor.execute("SELECT * FROM news ORDER BY id DESC")
     return cursor.fetchall()
+
+def deleteNews(id):
+    cursor = cnx.cursor()
+    sql = "DELETE FROM news WHERE id=%s"
+    val = (id, )
+    cursor.execute(sql, val)
+    return True
